e8b5b51618d25ec60930f99002186e04b2e9e003
MrPanuchi <40564816+MrPanuchi@users.noreply.github.com>
2018-06-27 15:03:50 +0300

    Update 4a.py

--- 4a.py
+++ 4a.py
@@ -1,22 +1,72 @@
-import random
+import random;
+
+def check_text (text_):
+  text_local_split = text_.split('\n\t');
+  for i in range (0,len(text_local_split)):
+    check_str(text_local_split[i],dict_1);
+  rndmz (dict_1);
+
+def check_str (str_,dict_):
+  local_str = str_.split();
+  local_dict = {'BEGIN':[]};
+  last_key = 'BEGIN';
+  for i in range (0,len(local_str)):
+    local_dict.update({last_key:[local_str[i]]});
+    if (last_key in dict_):
+      buf = dict_.get(last_key);
+      buf = buf + local_dict.get(last_key);
+      dict_.update({last_key:buf});
+    else:
+      dict_.update({last_key:local_dict.get(last_key)})
+    last_key = str(local_str[i]);
+  'local_dict.update({last_key:''END''});'
+  dict_.update({last_key:['END']});
+
+def rndmz (dict_):
+  text_rndmz = '';
+  buf_list = dict_.get('BEGIN');
+  buf_word = '';
+  while (True):
+    ch = random.randint(0,(len(buf_list)-1));
+    if ('END' == str(buf_list[ch])):
+      break;
+    else:
+      buf_word = buf_list[ch];
+      buf_list = dict_.get(buf_word);
+      text_rndmz = text_rndmz + buf_word + ' ';
+  print (text_rndmz);
 
 
-def ran(word, dict_):
-	list_ = dict_.get(word)
-	rand = random.randint(0, len(list_) - 1)
-	oword = list_[rand]
-	return oword
+text_1 = '''Мороз и солнце, день чудесный!
+	Еще ты дремлешь, друг прелестный –
+	Пора, красавица, проснись;
+	Открой сомкнуты негой взоры
+	Навстречу северной Авроры,
+	Звездою севера явись!
+	Вечор, ты помнишь, вьюга злилась,
+	На мутном небе мгла носилась;
+	Луна, как бледное пятно,
+	Сквозь тучи мрачные желтела,
+	И ты печальная сидела –
+	А нынче... погляди в окно:
+	Под голубыми небесами
+	Великолепными коврами,
+	Блестя на солнце, снег лежит;
+	Прозрачный лес один чернеет,
+	И ель сквозь иней зеленеет,
+	И речка подо льдом блестит.
+	Вся комната янтарным блеском
+	Озарена. Веселым треском
+	Трещит затопленная печь.
+	Приятно думать у лежанки.
+	Но знаешь: не велеть ли в санки
+	Кобылку бурую запречь?
+	Скользя по утреннему снегу,
+	Друг милый, предадимся бегу
+	Нетерпеливого коня
+	И навестим поля пустые,
+	Леса, недавно столь густые,
+	И берег, милый для меня. '''
 
-
-a = {
-    'собака': ['туда-ка', 'собака', 'сюда-ка'],
-    'изучает': ['обучает', 'тренирует', 'развивает', 'изучает'],
-    'дзю-до': ['дзю-до', 'дзю-после', 'мяу тай', 'самбо', 'несамобо']
-}
-b = 'собака изучает дзю-до'
-print(b)
-c = b.split()
-d = ''
-for i in range(0, len(c)):
-	d = d + ran(c[i], a) + ' '
-print(d)
+dict_1 = {};
+check_text(text_1);
